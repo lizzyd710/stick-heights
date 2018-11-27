566931f955461bc2e5c54fc86d3e23f92dd29d84
Elizabeth Sherrock <lizzyd710@gmail.com>
2018-11-27 14:46:27 -0500

    Created a method for blob tracking rather than using corners like in my optical_flow method.

--- src/stick heights.py
+++ src/stick heights.py
@@ -85,6 +85,44 @@ def optical_flow(capture):
         old_gray = frame_gray.copy()
         p0 = good_new.reshape(-1, 1, 2)
 
+##################################
+# This is copied from an opencv tutorial to use as a skeleton.
+# https://docs.opencv.org/3.4/db/df8/tutorial_py_meanshift.html
+##################################
+
+def blob_track(capture):
+    # take first frame of the video
+    ret, frame = capture.read()
+    # setup initial location of window
+    r, h, c, w = 250, 90, 400, 125  # simply hardcoded the values
+    track_window = (c, r, w, h)
+    # set up the ROI for tracking
+    roi = frame[r:r + h, c:c + w]
+    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
+    roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
+    cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+    # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+    term_crit = (cv2.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1)
+    while (1):
+        ret, frame = capture.read()
+        if ret == True:
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+            # apply meanshift to get the new location
+            ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+            # Draw it on image
+            pts = cv2.boxPoints(ret)
+            pts = np.intp(pts)
+            img2 = cv2.polylines(frame, [pts], True, 255, 2)
+            cv2.imshow('img2', img2)
+            k = cv2.waitKey(60) & 0xff
+            if k == 27:
+                break
+            #else:
+                #cv2.imwrite(chr(k) + ".jpg", img2)
+        else:
+            break
 
 optical_flow(cap)
 cv2.destroyAllWindows()
